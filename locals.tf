2395e9d16c18378fb853d24af4c9bd1c4b770f74
jason-drouhard <jason.drouhard@stratusgrid.com>
2021-05-28 14:14:23 -0400

    Defined common_tags in locals.tf

--- locals.tf
+++ locals.tf
@@ -3,5 +3,9 @@ locals {
   default_build_command = <<BUILDCOMMAND
   msbuild /nologo /p:Configuration=${var.build_configuration} /p:DeployOnBuild=true /p:WebPublishMethod=Package /p:PackageAsSingleFile=true /p:platform="x64" /p:SkipInvalidConfigurations=true /p:DesktopBuildPackageLocation="C:\${var.artifact_path}\${var.codebuild_name}.zip"
   BUILDCOMMAND
+
+  common_tags = merge(var.input_tags, {
+    "ModuleSourceRepo" = "github.com/StratusGrid/terraform-aws-codebuild-webdeploy-package"
+  })
 }
 
